86222fc3db9a0cf56c330d6190185bb538f64caf
lia102 <hathathat95@gmail.com>
2023-07-06 13:44:25 +0900

    add : sh - ohlcv 기본 연동

--- Spring/src/main/java/com/hanaonestock/stock/service/OhlcvServiceImpl.java
+++ Spring/src/main/java/com/hanaonestock/stock/service/OhlcvServiceImpl.java
@@ -1,4 +1,25 @@
 package com.hanaonestock.stock.service;
 
-public class OhlcvServiceImpl {
+import com.hanaonestock.stock.model.dao.OhlcvMapper;
+import com.hanaonestock.stock.model.dto.Ohlcv;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OhlcvServiceImpl implements OhlcvService{
+
+    private OhlcvMapper ohlcvMapper;
+
+    @Autowired
+    public OhlcvServiceImpl(OhlcvMapper ohlcvMapper){
+        this.ohlcvMapper = ohlcvMapper;
+    }
+
+    @Override
+    public List<Ohlcv> findAll(){
+        return ohlcvMapper.findAll();
+    }
+
 }
