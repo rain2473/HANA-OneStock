908289d751f1bd1e013effca23a53874c26c556f
SEUNGHEE96 <dnffktmdgml@gmail.com>
2023-07-10 21:21:47 +0900

    Add : 차트 라이브러리 완성

--- Spring/src/main/java/com/hanaonestock/transaction/controller/TransactionController.java
+++ Spring/src/main/java/com/hanaonestock/transaction/controller/TransactionController.java
@@ -1,10 +1,21 @@
 package com.hanaonestock.transaction.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hanaonestock.stock.model.dto.Ohlcv;
 import com.hanaonestock.transaction.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 @Controller
 public class TransactionController {
@@ -21,6 +32,21 @@ public class TransactionController {
         ModelAndView mav = new ModelAndView();
         mav.setViewName("trading");
         return mav;
+    }
+
+    @RequestMapping(value = "/trading", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<String> getStockChartData(@RequestBody Map<String, String> requestMap) {
+        String isin = requestMap.get("isin");
+        List<Ohlcv> ohlcvList = transactionService.search(isin);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json;
+        try {
+            json = objectMapper.writeValueAsString(ohlcvList);
+        } catch (JsonProcessingException e) {
+            return new ResponseEntity<>("Error processing JSON", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(json, HttpStatus.OK);
     }
 
 }
