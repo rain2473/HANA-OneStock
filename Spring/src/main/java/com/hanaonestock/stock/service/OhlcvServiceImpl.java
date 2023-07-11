ec885e8b9636908a9999a33d2ad897205845e6dc
pkd98 <baum12345@naver.com>
2023-07-11 15:38:57 +0900

    Modify: 주석 추가

--- Spring/src/main/java/com/hanaonestock/stock/service/OhlcvServiceImpl.java
+++ Spring/src/main/java/com/hanaonestock/stock/service/OhlcvServiceImpl.java
@@ -14,8 +14,9 @@ public class OhlcvServiceImpl implements OhlcvService{
 
     private OhlcvMapper ohlcvMapper;
     private LocalDate currentDate = LocalDate.now();
+    private LocalDate previousDate = currentDate.minusDays(1);
     private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
-    private String now = currentDate.format(formatter);
+    private String previousDay = previousDate.format(formatter);
 
 
     @Autowired
@@ -23,29 +24,43 @@ public class OhlcvServiceImpl implements OhlcvService{
         this.ohlcvMapper = ohlcvMapper;
     }
 
+    /**
+     *  테스트용 모든 데이터 검색
+     */
     @Override
     public List<Ohlcv> findAll(){
         return ohlcvMapper.findAll();
     }
-
+    /**
+     *  상승률 탑 5 종목
+     */
     @Override
     public List<Ohlcv> findRisingTop5ByDate() {
-        return ohlcvMapper.findRisingTop5ByDate(now);
+        return ohlcvMapper.findRisingTop5ByDate(previousDay);
     }
 
+    /**
+     *  하락률 탑 5 종목
+     */
     @Override
     public List<Ohlcv> findFallingTop5ByDate() {
-        return ohlcvMapper.findFallingTop5ByDate(now);
+        return ohlcvMapper.findFallingTop5ByDate(previousDay);
     }
 
+    /**
+     *  거래랑 상위 5 종목
+     */
     @Override
     public List<Ohlcv> findVolumeTop5ByDate() {
-        return ohlcvMapper.findVolumeTop5ByDate(now);
+        return ohlcvMapper.findVolumeTop5ByDate(previousDay);
     }
 
+    /**
+     *  거래대금 상위 5 종목
+     */
     @Override
     public List<Ohlcv> findAmountTop5ByDate() {
-        return ohlcvMapper.findAmountTop5ByDate(now);
+        return ohlcvMapper.findAmountTop5ByDate(previousDay);
     }
 
 }
--- Spring/src/test/java/com/hanaonestock/demo/stock/OhlcvTest.java
+++ Spring/src/test/java/com/hanaonestock/demo/stock/OhlcvTest.java
@@ -56,7 +56,7 @@ public class OhlcvTest {
             }
 
         } catch (Exception e) {
-                
+
         }
 
     }
